37d701aab89dd998c1ed8276259e4c3277e5ec2a
MichaelAjith <43992439+MichaelAjith@users.noreply.github.com>
2018-11-08 23:04:31 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+  <head>
+    <title>SAD</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <link rel="stylesheet" type="text/css" href="oes.css"/>
+  </head>
+  <body>
+      
+      
+      <div id="container">
+            
+                <div class="header">
+                <img style="margin:10px 2px 2px 10px;float:left;" height="80" width="200" src="images/logo.gif" alt="OES"/><h3 class="headtext"> &nbsp;STUDENT ASSESSMENT DESIGN </h3><h4 style="color:#ffffff;text-align:center;margin:0 0 5px 5px;"><i>...because Examination Matters</i></h4>
+            </div>
+     <form id="homeform" action="home.php" method="post">
+      <div class="menubar">
+          
+       <ul id="menu">
+                    
+           <li><div class="aclass"><a href="indexs.php" title="Click here for student login">Student</a></div></li>
+           <li><div class="aclass"><a href="indext.php" title="Click here for student login">Faculty</a></div></li>
+           <li ><div class="aclass" ><a href="indexa.php" title="Click here for admin login" >Admin</a></div></li>
+                        
+                    </ul>
+
+      </div>
+     
+
+
+      </div>
+       </form>
+          <div>
+              <img src="images/clg1.jpg" height="470" width="1350"></img>
+          </div>
+
+    <div id="footer">
+          <p style="font-size:70%;color:#ffffff;"> Developed By-<b>Michael</b><br/> </p>
+      </div>
+      </div>
+  </body>
+</html>
